eb062bb233ef8dd3c0a9695dfbad2c3e1a9e3ceb
garcialuis <garcialuisdev@gmail.com>
2021-02-21 14:30:13 -0800

    chore: moved user template example to playground/
    Moved the user template example to its own file, out of App.js
    in the playground folder. App.js will now be used for the
    main app (indecision app).

--- src/App.js
+++ src/App.js
@@ -19,31 +19,9 @@ const template = (
   </div>
 )
 
-// Exercise / Template Two:
-
-const user = {
-  name: "Mike Wasauski",
-  age: 25,
-  location: 'New York'
-}
-
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>
-  }
-}
-
-const templateTwo = (
-  <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-  </div>
-)
-
 function App() {
   return (
-    templateTwo
+    template
   );
 }
 
