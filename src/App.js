9c04f0e8df586ad9c7fb95ab38ea3dd325c737d8
garcialuis <garcialuisdev@gmail.com>
2021-02-21 14:17:40 -0800

    refactor: Modified App.js and index.js to show manual data binding
    In App.js a new template (templateThree) was added with a counter.
    The counter is incremented or decremented or reset with button
    clicks. In order to show the updated counter, we moved the rendering
    of the app from index.js to App.js in the renderTemplateThree function.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import ReactDOM from 'react-dom';
+
+// Exercise/Template One: 
 
 const app = {
   title: 'Indecision App',
@@ -16,6 +19,8 @@ const template = (
     </ol>
   </div>
 )
+
+// Exercise / Template Two:
 
 const user = {
   name: "Mike Wasauski",
@@ -37,11 +42,56 @@ const templateTwo = (
   </div>
 )
 
+// Exercise / Template Three:
+
+let count = 0
+
+// All three of the functions for the button clicks 
+// call renderTemplateThree at the end so that the 
+// count can be rerendered to the screen:
+const addOne = () => {
+  count++
+  console.log('add one')
+  renderTemplateThree()
+}
+
+const subtractOne = () => {
+  count--
+  console.log('minus one')
+  renderTemplateThree()
+}
+
+const resetCount = () => {
+  count = 0
+  console.log('reset')
+  renderTemplateThree()
+}
+
+
+// Gets called every time we want to render our templateThree.
+// For example, after every button click to change the count the templateThree 
+// will be rendered. 
+const renderTemplateThree = () => {
+  const templateThree = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button id='button-id' className='button' onClick={addOne}>+1</button>
+      <button className='button' onClick={subtractOne}>-1</button>
+      <button className='button' onClick={resetCount}>reset</button>
+    </div>
+  )
+
+  var appRoot = document.getElementById('root')
+  ReactDOM.render(templateThree, appRoot);
+}
 
 function App() {
   return (
-    template
+    templateTwo
   );
 }
+
+// Initially renders our templateThree:
+renderTemplateThree()
 
 export default App;
