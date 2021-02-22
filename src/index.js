763e4f472cb64e59b0986123efa8378de02c5e70
garcialuis <garcialuisdev@gmail.com>
2021-02-22 13:36:34 -0800

    feat: Added 'What should I do button?' to choose an option for the user
    There has been a button added to the App.js that is responsible for
    choosing an option out of the user's created list. This button also
    has a disabled feature that allows the button to be clicked if there
    are options but disabled if the user has not added any options to
    the list.

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ var template = (
     <App />
   </React.StrictMode>
 )
-var appRoot = document.getElementById('root')
 
-ReactDOM.render(template, appRoot);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
