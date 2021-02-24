490c85aa19c646c4c9811d25d2b31e945abef426
garcialuis <garcialuisdev@gmail.com>
2021-02-23 16:57:56 -0800

    refactor: Moved indecision app (jsx) to playground dir.
    The current indecision app is all in jsx, there are no components
    that make it up. As a result, it was moved to the playground
    directory, as the app.js is now going to start forming the app
    using components.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ var template = (
   </React.StrictMode>
 )
 
+var appRoot = document.getElementById('root')
+ReactDOM.render(template, appRoot);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
