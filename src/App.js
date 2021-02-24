4802c1647d5c6e51689087d8c501f3d2522398e0
garcialuis <garcialuisdev@gmail.com>
2021-02-23 17:21:41 -0800

    feat: Setting up functional components for Indecision App
    Prevously the indecision app was all made up of JSX. Now the goal
    is to split them into components. The end goal is to have components
    all in their separate files, but for now they will live in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
-const emptyTemplate = (
-  <div></div>
-)
+const Header = () => {
+  return (
+    <div>
+      <h1>Indecision</h1>
+      <h2>Put your life in the hands of a computer</h2>
+    </div>
+  );
+}
+
+const Action = () => {
+  return (
+    <div>
+      <button>What should I do?</button>
+    </div>
+  );
+}
+
+const Options = () => {
+  return (
+    <div>
+      <p>Option component here</p>
+      <Option />
+    </div>
+  );
+}
+
+const Option = () => {
+  return (
+    <div>
+      This is an option
+    </div>
+  );
+}
+
+const AddOption = () => {
+  return (
+    <div>
+      <p>AddOption component here</p>
+    </div>
+  );
+}
+
+const IndecisionApp = () => {
+  return (
+    <div>
+      <Header />
+      <Action />
+      <Options />
+      <AddOption />
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    emptyTemplate
+    <IndecisionApp />
   );
 }
 
