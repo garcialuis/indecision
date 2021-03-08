bcef516a086a7316f2fd6965c3f909f0b55f5078
garcialuis <garcialuisdev@gmail.com>
2021-03-08 13:15:28 -0800

    refactor: refactored components to not implicit use the 'return' keyword
    Simple components such as Header, Topic, and Action have been refactored
    to no longer use the 'return' keyword explicitly.

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
-const Action = ({hasOptions, handlePick}) => {
-    return (
-        <div>
-          <button 
-            disabled={!hasOptions} 
-            onClick={handlePick}>
-              What should I do?
-          </button>
-        </div>
-    );
-}
+const Action = ({hasOptions, handlePick}) => (
+  <div>
+    <button 
+      disabled={!hasOptions} 
+      onClick={handlePick}>
+        What should I do?
+    </button>
+  </div>
+)
 
 export default Action
